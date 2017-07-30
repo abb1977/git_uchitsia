9e67ffb2f1a42afe1ac9decb1c91f3ccd749eced
andrey <abb1977@mail.ru>
2017-07-30 22:26:37 +0300

    piatyi

--- menu/simple2.php
+++ menu/simple2.php
@@ -9,7 +9,7 @@
 	<li><a href='<?php $_SERVER["DOCUMENT_ROOT"]?>/pages/o_saite.php'>О сайте</a></li>
 	<li><a href='<?php $_SERVER["DOCUMENT_ROOT"]?>/pages/kontakty.php'>Контакты</a></li>
 	<li><a href='<?php $_SERVER["DOCUMENT_ROOT"]?>/index.php'>gdgdfgdgя</a></li>
-	<li><a href='<?php $_SERVER["DOCUMENT_ROOT"]?>/pages/o_saite.php'>jghjgfе</a></li>
-	<li><a href='<?php $_SERVER["DOCUMENT_ROOT"]?>/pages/kontakty.php'>uoytvvmv</a></li>
+	<li><a href='<?php $_SERVER["DOCUMENT_ROOT"]?>/pages/o_saite.php'>jghjfsdfgfе</a></li>
+	<li><a href='<?php $_SERVER["DOCUMENT_ROOT"]?>/pages/kontakty.php'>uoytdsfsvvmv</a></li>
 	<li><a href='<?php $_SERVER["DOCUMENT_ROOT"]?>'></a></li>
 </ul>
